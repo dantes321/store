a9ac73c6e603168318490035f4af1806786f5627
dantes321 <alexkardash97@gmail.com>
2021-04-16 16:25:15 +0300

    213211111123

--- src/components/NewBox/NewBox.jsx
+++ src/components/NewBox/NewBox.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import s from './NewBox.module.scss';
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 
 const NewBox = () => {
+    useEffect(() =>{
+        Aos.init({duration:1000,once:true})
+    },[])
     return (
         <div className={s.newBox}>
             <div className={s.boxContainer}>
-                <div className={s.title}>Нужен четкий Сюрприз-Бокс?</div>
-                <div className={s.subtitle}>Наш сервис случайных подарков поможет Вам!</div>
+                <div data-aos = 'fade-right' className={s.title}>Нужен четкий Сюрприз-Бокс?</div>
+                <div  data-aos = 'fade-left' className={s.subtitle}>Наш сервис случайных подарков поможет Вам!</div>
                 <a href="#">Выбрать бокс</a>
             </div>
         </div>
--- src/components/Store/Boxes/Boxes.jsx
+++ src/components/Store/Boxes/Boxes.jsx
@@ -6,10 +6,10 @@ import 'aos/dist/aos.css';
 const Boxes = (props) => {
 
     useEffect(() =>{
-        Aos.init({duration:2000,once:true})
+        Aos.init({duration:1000,once:true})
     },[])
 
-    return props.boxes.map(el => <div data-aos='slide-up' key={el.id} className={s.box}>
+    return props.boxes.map(el => <div data-aos='fade-up' key={el.id} className={s.box}>
         <img src={el.img} alt=""/>
         <div className={s.title}>{el.title}</div>
         <div className={s.price}>{el.price} грн</div>
