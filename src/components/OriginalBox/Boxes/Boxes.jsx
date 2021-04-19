683118cef8996d245c8bb74535538aa54176186d
dantes321 <alexkardash97@gmail.com>
2021-04-19 14:36:28 +0300

    "endOfLine":"auto"

--- src/components/OriginalBox/Boxes/Boxes.jsx
+++ src/components/OriginalBox/Boxes/Boxes.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'aos/dist/aos.css';
-import ModalWindow from '../../ModalWindow/ModalWindow';
-
 import s from './Boxes.module.scss';
 
-const Boxes = (props) => {
-  const [active, isActive] = useState(false);
-
-  return props.boxes.map((el) => (
+const Boxes = (props) =>
+  props.boxes.map((el) => (
     <div ata-aos-duration={1100} data-aos={el.aos} key={el.id} className={s.boxes}>
       <img src={el.img} alt='' />
+
       <div className={s.title}>{el.title}</div>
       <div className={s.price}>{el.price} грн</div>
-      <a href='/#' onClick={() => isActive(true)}>
-        Выбрать
-      </a>
-      <ModalWindow active={active} isActive={isActive} />
+      <a href='/#'>Выбрать</a>
     </div>
   ));
-};
 
 export default Boxes;
